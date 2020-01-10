ffb54d38e7e2d5d2552cf4e07e9f257755edc6e1
Jeffrey Wang <wangjr@Pragmatics.com>
2020-01-10 12:04:45 -0500

    add python files and buildspec

---
+++ tdd_exercise.py
@@ -0,0 +1,24 @@
+def longestCommonSubstring(s1: str, s2: str) -> str:
+	D = [[] for i in range(len(s1))]
+
+	common = ""
+	for i in range(len(D)):
+		if i < len(s1) and i < len(s2) and s1[i] == s2[i]:
+			common += s1[i]
+		else:
+			return common
+	return common
+
+
+import unittest
+
+param_list = [('aa', 'aa', 'aa'), ('aa', 'aa3', 'aa')]
+class TestStringMethods(unittest.TestCase):
+
+	def test_start(self):
+		for p1, p2, res in param_list:
+			with self.subTest(msg="Checking if lcs( p1, p2 ) = res", p1=p1, p2=p2, res=res):
+				self.assertEqual(longestCommonSubstring(p1, p2), res)
+
+if __name__ == "__main__":
+	unittest.main()
